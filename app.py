5c56667f55e1e7739ff30dafbc854ba16cfa5ce4
Onur Aykac <onuraykac@gmail.com>
2016-12-15 12:22:05 +0200

    python buildpack and web server via flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, redirect, url_for, send_from_directory
+
+
+app = Flask(__name__, static_folder='site')
+
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    return app.send_static_file(path)
+
+
+if __name__ == "__main__":
+    app.run()
